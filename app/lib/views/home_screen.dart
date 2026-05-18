095df790ab3f1a93fac557cfbe4d5ba0faacea70
BRYCENVN\nvt_dung <nvt_dung@brycen.com.vn>
2026-05-18 16:41:47 +0700

    refactor + hidden floatingActionButtonLocation when open virtual keyboard

--- app/lib/views/home_screen.dart
+++ app/lib/views/home_screen.dart
@@ -1,32 +1,31 @@
 import 'package:app/core/theme/app_colors.dart';
 import 'package:app/views/dashboard.dart';
+import 'package:app/views/login_screen.dart';
 import 'package:app/views/password_create_screen.dart';
 import 'package:app/views/password_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../providers/password_provider.dart';
 
 class HomeScreen extends StatefulWidget {
+  bool isRegistered;
+  HomeScreen(this.isRegistered);
   @override
-  // _HomeScreenState createState() => _HomeScreenState();
   _MasterBottomNavState createState() => _MasterBottomNavState();
-  // _MasterNotchedBarState createState() => _MasterNotchedBarState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  //PasswordScreen
-
+class _MasterBottomNavState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
   // Danh sách các màn hình tương ứng với menu
-  static final List<Widget> _widgetOptions = <Widget>[
+  List<Widget> get _widgetOptions => <Widget>[
+    Center(child: DashboardScreen()),
     Center(child: PasswordScreen()),
-    const Center(child: Text('Tìm kiếm', style: TextStyle(fontSize: 24))),
-    const Center(child: Text('Thông báo', style: TextStyle(fontSize: 24))),
-    const Center(child: Text('Cá nhân', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Comming soon', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Comming soon', style: TextStyle(fontSize: 24))),
+    Center(child: CreatePasswordScreen(onGoToSearch: (index) => _changeTab(index))),
   ];
 
-  void _onItemTapped(int index) {
+  // Hàm thay đổi tab
+  void _changeTab(int index) {
     setState(() {
       _selectedIndex = index;
     });
@@ -34,54 +33,37 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Nếu viewInsets.bottom > 0 nghĩa là bàn phím ảo đang mở
+  bool isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
+
     return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search_rounded), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications_rounded), label: 'Alerts'),
-          BottomNavigationBarItem(icon: Icon(Icons.person_rounded), label: 'Profile'),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blueAccent, // Màu khi được chọn
-        unselectedItemColor: Colors.grey,     // Màu khi không chọn
-        onTap: _onItemTapped,
-        type: BottomNavigationBarType.fixed, // Giữ vị trí icon cố định
-        showSelectedLabels: true,
-        showUnselectedLabels: false,         // UX: Chỉ hiện label khi được chọn để đỡ rối mắt
-        elevation: 10,
-      ),
-    );
-  }
-}
-
-class _MasterBottomNavState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-
-  // Danh sách các màn hình tương ứng với menu
-  static final List<Widget> _widgetOptions = <Widget>[
-    Center(child: DashboardScreen()),
-    Center(child: PasswordScreen()),
-    const Center(child: Text('Comming soon', style: TextStyle(fontSize: 24))),
-    const Center(child: Text('Comming soon', style: TextStyle(fontSize: 24))),
-    Center(child: CreatePasswordScreen()),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      // resizeToAvoidBottomInset: false,
       appBar: AppBar(
         //Trình quản lý mật khẩu
         title: Text("---", 
           style: TextStyle(color: AppColors.primary, fontWeight: FontWeight.w500)
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout, color: AppColors.primary,),
+            onPressed: () {
+              widget.isRegistered = false;
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/login', // Trang bạn muốn chuyển đến
+                (route) => false, // Điều kiện xóa: 'false' nghĩa là xóa SẠCH CÁC TRANG CŨ
+              );
+            },
+          ),
+        ],
         // elevation: 1.0,
         // shadowColor: Colors.black,
         backgroundColor: const Color.fromARGB(255, 255, 255, 255)
       ),
       body: Padding(padding: EdgeInsets.symmetric(horizontal: 10), child: _widgetOptions.elementAt(_selectedIndex)),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: isKeyboardOpen 
+        ? null 
+        : FloatingActionButton(
           onPressed: () {
             // _showAddPasswordDialog(context);
             // CreatePasswordScreen();
@@ -166,90 +148,6 @@ class _MasterBottomNavState extends State<HomeScreen> {
                   fontSize: 13,
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _showAddPasswordDialog(BuildContext context) {
-    final nameController = TextEditingController();
-    final userController = TextEditingController();
-    final passController = TextEditingController();
-    bool isSaving = false;
-
-    showDialog(
-      context: context,
-      barrierDismissible: false, // Ngăn người dùng tắt dialog khi đang lưu
-      builder: (context) => StatefulBuilder( // Dùng StatefulBuilder để cập nhật trạng thái trong Dialog
-        builder: (context, setState) => AlertDialog(
-          title: const Text("Thêm mật khẩu mới"),
-          content: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    labelText: "Tên dịch vụ",
-                    hintText: "Ví dụ: Facebook, Gmail...",
-                  ),
-                ),
-                TextField(
-                  controller: userController,
-                  decoration: const InputDecoration(labelText: "Tên đăng nhập"),
-                ),
-                TextField(
-                  controller: passController,
-                  obscureText: true,
-                  decoration: const InputDecoration(labelText: "Mật khẩu"),
-                ),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: isSaving ? null : () => Navigator.pop(context),
-              child: const Text("Hủy"),
-            ),
-            ElevatedButton(
-              onPressed: isSaving 
-                ? null 
-                : () async {
-                    if (nameController.text.isEmpty || passController.text.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("Vui lòng nhập đủ thông tin!")),
-                      );
-                      return;
-                    }
-
-                    setState(() => isSaving = true);
-
-                    try {
-                      // Gọi Provider để mã hóa và lưu vào Secure Storage
-                      await Provider.of<PasswordProvider>(context, listen: false).addPassword(
-                        nameController.text,
-                        userController.text,
-                        passController.text,
-                      );
-
-                      if (context.mounted) {
-                        Navigator.pop(context);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("Đã lưu mật khẩu thành công!")),
-                        );
-                      }
-                    } catch (e) {
-                      setState(() => isSaving = false);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Lỗi: $e")),
-                      );
-                    }
-                  },
-              child: isSaving 
-                ? const SizedBox(width: 20, height: 20, child: CircularProgressIndicator(strokeWidth: 2))
-                : const Text("Lưu lại"),
             ),
           ],
         ),
